fcc12cfb2d3c1dee3feae88ae19b93fe212751a9
joycerijs <j.rijs@student.tudelft.nl>
2023-04-13 12:21:49 +0200

    first statistic analysis

--- Statistics.py
+++ Statistics.py
@@ -17,17 +17,17 @@ def statistics(rest_data, stress_data, sign_features_dfs):
         _, p = stats.ttest_ind(rest_data[key], stress_data[key])   # Perform the Student's t-test
         df_p.loc[df_p['Features'] == key, 'P-value'] = p    # Fill dataframe with p-values
         # Calculate the mean and std for the two populations and fill in dataframe
-        mean_ID = np.round(stress_data[key].mean(), decimals=2)
-        std_ID = np.round(stress_data[key].std(), decimals=2)
-        mean_no_ID = np.round(rest_data[key].mean(), decimals=2)
-        std_no_ID = np.round(rest_data[key].std(), decimals=2)
-        df_p.loc[df_p['Features'] == key, 'Mean ± std ID'] = f'{mean_ID} ± {std_ID}'
-        df_p.loc[df_p['Features'] == key, 'Mean ± std no ID'] = f'{mean_no_ID} ± {std_no_ID}'
+        mean_stress = np.round(stress_data[key].mean(), decimals=2)
+        std_stress = np.round(stress_data[key].std(), decimals=2)
+        mean_rest = np.round(rest_data[key].mean(), decimals=2)
+        std_rest = np.round(rest_data[key].std(), decimals=2)
+        df_p.loc[df_p['Features'] == key, 'Mean ± std stress'] = f'{mean_stress} ± {std_stress}'
+        df_p.loc[df_p['Features'] == key, 'Mean ± std rest'] = f'{mean_rest} ± {std_rest}'
 
     # Find significant p-values by Holm-Bonferroni:
     df_p_sorted = df_p.sort_values(by=['P-value'])    # Sort the values by p-values
     df_p_sorted['Rank'] = range(1, len(df_p_sorted)+1)    # Rank the features
-    df_p_sorted['Significance level'] = 0.05/(len(df_p_sorted)+1-df_p_sorted['Rank'])    # Calculate the significance level per feature
+    df_p_sorted['Significance level'] = 0.05  # /(len(df_p_sorted)+1-df_p_sorted['Rank'])    # Calculate the significance level per feature
     df_p_sorted['Significant'] = np.where(df_p_sorted['P-value'] < df_p_sorted['Significance level'], 'Yes', 'No')    # Find which features are significant
 
     # Create dataframe with significant features only and create table for visualisation
@@ -45,8 +45,8 @@ def statistics(rest_data, stress_data, sign_features_dfs):
 
 sign_features_dfs = []
 
-file_rest = pd.read_table('F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 3/Neuro VR/rust_data_gecombineerd.csv', delimiter=";", decimal=',')
-file_stress = pd.read_table('F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 3/Neuro VR/stress_data_gecombineerd.csv', delimiter=";", decimal=',')
+file_rest = pd.read_table('F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 3/Neuro VR/rust_data_gecombineerd.csv', delimiter=",")
+file_stress = pd.read_table('F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 3/Neuro VR/stress_data_gecombineerd.csv', delimiter=",")
 
 sign, sign_features_dfs = statistics(file_rest, file_stress, sign_features_dfs)
 
