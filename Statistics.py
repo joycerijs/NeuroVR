e5ec3aa3330930bb78a1ddf4e1183991db87fb67
joycerijs <j.rijs@student.tudelft.nl>
2023-05-02 17:35:05 +0200

    opgeschoond. samengevoegd met data_analysis

--- Statistics.py
+++ Statistics.py
@@ -10,7 +10,7 @@ from statistics import mean
 from statistics import stdev
 
 
-def statistics(rest_data, stress_data, sign_features_dfs):
+def statistics(rest_data, stress_data):
     # Student's t-test for continuous data
     df_p = pd.DataFrame({'Features': rest_data.keys()})
     for key in rest_data.keys():
@@ -24,33 +24,20 @@ def statistics(rest_data, stress_data, sign_features_dfs):
         df_p.loc[df_p['Features'] == key, 'Mean ± std stress'] = f'{mean_stress} ± {std_stress}'
         df_p.loc[df_p['Features'] == key, 'Mean ± std rest'] = f'{mean_rest} ± {std_rest}'
 
-    # Find significant p-values by Holm-Bonferroni:
+    # Find significant p-values and create table with significant features only:
     df_p_sorted = df_p.sort_values(by=['P-value'])    # Sort the values by p-values
-    df_p_sorted['Rank'] = range(1, len(df_p_sorted)+1)    # Rank the features
-    df_p_sorted['Significance level'] = 0.05  # /(len(df_p_sorted)+1-df_p_sorted['Rank'])    # Calculate the significance level per feature
-    df_p_sorted['Significant'] = np.where(df_p_sorted['P-value'] < df_p_sorted['Significance level'], 'Yes', 'No')    # Find which features are significant
+    df_p_sorted['Significant'] = np.where(df_p_sorted['P-value'] < 0.05, 'Yes', 'No')    # Find which features are significant
+    df_p_for_table = df_p_sorted.loc[df_p_sorted['Significant'] == 'Yes']
 
-    # Create dataframe with significant features only and create table for visualisation
-    df_p_sign = df_p_sorted.loc[df_p_sorted['Significant'] == 'Yes']
-    df_p_for_table = df_p_sign.drop(['Rank'], axis=1)
+    return df_p_for_table
 
-    # Append the dataframe with significant features to a list for every fold. In this list, the dataframes for the 10 folds are stored.
-    sign_features_dfs.append(df_p_for_table)
-    print(np.round(df_p_for_table['P-value'], decimals=3))
-
-    # Create list of significant features that can be used for model creation
-    sign = df_p_sign['Features'].tolist()
-
-    return sign, sign_features_dfs
-
-sign_features_dfs = []
 
 file_rest = pd.read_table('F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 3/Neuro VR/rust_data_gecombineerd.csv', delimiter=",")
 file_stress = pd.read_table('F:/Documenten/Universiteit/Master_TM+_commissies/Jaar 3/Neuro VR/stress_data_gecombineerd.csv', delimiter=",")
 
-sign, sign_features_dfs = statistics(file_rest, file_stress, sign_features_dfs)
+sign_features_dfs = statistics(file_rest, file_stress)
 
-# print(sign)
+print(sign_features_dfs)
 
 
 
